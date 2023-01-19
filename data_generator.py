e7e7e7f02d7a9b7ebdfbbf39d2425233309c4205
César Salcedo <cesar.salcedo@utec.edu.pe>
2023-01-19 11:18:29 -0500

    Added option to let data generator perform calls to stack that produce an error (pop on empty stack)

--- data_generator.py
+++ data_generator.py
@@ -6,25 +6,46 @@ import numpy as np
 # TOKEN_1 = 3
 # TOKEN_ERR = 4
 
-OP_PUSH = np.array([1, 0, 0, 0, 0])
-OP_POP  = np.array([0, 1, 0, 0, 0])
-TOKEN_0 = np.array([0, 0, 1, 0, 0])
-TOKEN_1 = np.array([0, 0, 0, 1, 0])
-TOKEN_ERR = np.array([0, 0, 0, 0, 1])
+# OP_PUSH = np.array([1, 0, 0, 0, 0])
+# OP_POP  = np.array([0, 1, 0, 0, 0])
+# TOKEN_0 = np.array([0, 0, 1, 0, 0])
+# TOKEN_1 = np.array([0, 0, 0, 1, 0])
+# TOKEN_ERR = np.array([0, 0, 0, 0, 1])
+
+OP_PUSH = "OP_PUSH"
+OP_POP  = "OP_POP"
+TOKEN_0 = "1"
+TOKEN_1 = "0"
+TOKEN_ERR = "ERR"
     
 
 
 class DataGenerator():
-    def __init__(self, push_prob=0.5):
-        self.push_prob = push_prob
+    def __init__(
+            self,
+            push_prob=0.5,
+            min_seq_len=1,
+            is_error_free=True,
+        ):
 
+        self.push_prob = push_prob
+        self.min_seq_len = min_seq_len
+        self.is_error_free = is_error_free
+        
+        self.reset()
+    
+    def reset(self):
         self.stack = []
         self.input = []
         self.output = []
 
     def sample(self):
-        self._push()
-
+        while len(self.input) < self.min_seq_len:
+            if self.is_error_free and len(self.stack) == 0:
+                self._push()
+            else:
+                self._random_op()
+        
         while len(self.stack) > 0:
             self._random_op()
         
@@ -39,7 +60,7 @@ class DataGenerator():
         self.input.append(value)
     
     def _pop(self):
-        value = self.stack.pop()
+        value = self.stack.pop() if len(self.stack) > 0 else TOKEN_ERR
 
         self.input.append(OP_POP)
         self.output.append(value)
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from data_generator import DataGenerator
 
-dg = DataGenerator(push_prob=0.5)
+dg = DataGenerator(
+    push_prob=0.5,
+    min_seq_len=10,
+    is_error_free=False,
+)
 
 data = dg.sample()
 
